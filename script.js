c2a5fc5e97d642f30a7b1c1f729287c652b42681
gregklamt <art@gregklamt.com>
2015-09-17 10:04:56 -0700

    initial commit, assignment 1 files

---
+++ script.js
@@ -0,0 +1,190 @@
+// STEP 1
+
+	/*var someMonth;
+	function theMonth();
+	const currentMonth = "January";
+	var summerMonth = ["June", "July", "August", "September"];
+	function fnMyLibrary();*/
+
+// STEP 2
+
+	/*
+	23.4		// a numeric literal expression, 
+	"Bazinga"	// a string literal expression, 
+	false		// a Boolean literal expression, 
+	null		// a null literal expression
+	*/
+
+// STEP 3	
+ 
+	// var customer = {firstName:"Geronimo", lastName:"Smythe", age:57, companyName:"Geronimo's Hoes"};
+
+	// var  myWeight = (220 + " lbs");
+
+
+// STEP 4
+
+	// var firstName, lastName, addAddress, addCity, addState, addZipCode, yourAge, referralSource, mayWeContactYou;
+
+// STEP 5 3 methods of declaring and assigning variables
+
+	/*var firstName = "Greg"; 
+	var lastName = "Klamt";
+	var adrAddress = "543 Gollum Lane";
+	var adrCity = "Hobbiton";
+	var adrState = "Middle Earth";
+	var adrZipCode = "92666";
+	var yourAge = "29";
+	var referralSource = "Friend";
+	var mayWeContactYou = "Yes";*/
+
+	/*var firstName = "Greg", 
+		lastName = "Klamt", 
+		adrAddress = "543 Gollum Lane", 
+		adrCity = "Hobbiton", 
+		adrState = "Middle Earth", 
+		adrZipCode = "92666", 
+		yourAge = "29", 
+		referralSource = "Friend", 
+		mayWeContactYou = "Yes";*/
+
+
+	// var firstName = "Greg", lastName = "Klamt", adrAddress = "543 Gollum Lane", adrCity = "Hobbiton", adrState = "Middle Earth", adrZipCode = "92666", yourAge = "29", referralSource = "Friend", mayWeContactYou = "Yes";
+
+//STEP 6 	3 variables
+	/*var myWeight = 200 + " lbs";
+	console.log (myWeight);*/
+	
+	/*var age = "fifty" + false;
+	console.log (age);*/
+
+	/*var myAge = 50 + false;
+	console.log (myAge);*/
+
+
+//STEP 7   Hoisting to create undefined
+	/*var hair1 = "Shorthair";
+	myDog = "My dog is " + hair2;
+	var hair2 = "Longhair";
+
+	console.log (myDog);*/
+
+
+//STEP 8
+
+	/*var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+	console.log(someString);*/
+
+
+// or, if you use double quotes in your js:
+
+	/*var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+	console.log(someString);*/
+
+
+//STEP 9
+
+	 var tomorrow = "Wednesday";
+	 var y = tomorrow;
+	 var x
+	 y = null;
+	 console.log(y);
+	 console.log(x);
+
+
+//STEP 10
+
+	// console.log (typeof "Troglodyte", typeof 546, typeof false, typeof {x:1, y:4}, typeof box);
+
+//STEP 11  Alert
+
+	 // alert ("Hello, " + "Greg Klamt" + ". Welcome to the JavaScript class!\n" + "\n(This alert has been corrected for punctuation!)");
+
+//STEP 12
+
+	/*	var name = "Greg Klamt";
+		alert ("Hello, " + name + ". Welcome to the JavaScript class!\n" + "\n(This alert has been corrected for punctuation!)");
+	*/
+
+// STEP 13
+
+	/*	var course = "JavaScript";
+		var name = "Greg Klamt";
+		alert ("Hello, " + name + ". Welcome to the " + course + " class!")
+	*/
+
+// STEP 14
+
+	/*var course = "JavaScript";
+	var name = "Greg Klamt";
+	alert ("Hello, " + name + ".\nWelcome to the " + course + " class!")
+*/
+
+// STEP 15
+
+	/*var course = "JavaScript";
+	var name = window.prompt ("Please enter your name.");
+	alert ("Hello, " + name + ".\nWelcome to the " + course + " class!");*/
+
+// STEP 16
+
+	/*var name = window.prompt ("Please enter your name.");
+	var course = window.prompt ("What course are you taking?");
+	alert ("Hello, " + name + ".\nWelcome to the " + course + " class!");*/
+
+// STEP 17
+
+	/*var x = 10;
+	var y = 20;
+	console.log (x + y);
+*/
+
+// STEP 18
+
+	/*var x = 20;
+	x += 20;
+	console.log (x);*/
+
+// STEP 19
+
+	/*var x = 20;
+	x *= 5;
+	console.log (x);*/
+
+
+// STEP 20
+
+	/*var x = 20 % 3;
+	x /= 1;
+	console.log (x);*/
+
+
+// STEP 21
+
+	/*var x = 20 % 3;
+	var y = 2;
+	console.log (x == y);*/
+
+
+// STEP 22
+
+	/*var dogs = "hairy",
+		cats = "lazy";
+	console.log (dogs == cats);*/
+
+// STEP 23
+
+	/*var widget = new Object();
+	console.log (typeof widget);*/
+
+// STEP 24
+
+	/*var widget = new Object();
+	console.log (widget instanceof Object);*/
+
+// STEP 25
+
+	/*var widget = new Object();
+	console.log (widget instanceof String);*/
+
+
